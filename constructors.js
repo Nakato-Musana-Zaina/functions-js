3b7fc38626352b59a39c31743fbd4dc1a37f92d3
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-04-25 19:10:22 +0300

    added answers to question1

--- constructors.js
+++ constructors.js
@@ -2,16 +2,52 @@
 // Question 1: (30 marks)
 // You are working on a web application for a car rental company. They want to keep track of their car inventory and rental information. Your task is to create a JavaScript class or function constructor for representing cars in their inventory and another one for representing rental information. 
 // Requirements: 
+
+Question1
 // 1) Create a Car class or function constructor that has the following properties:
 //  a. make (string): The make of the car, e.g., "Toyota". 
 // b. model (string): The model of the car, e.g., "Camry". 
 // c. year (number): The year the car was manufactured, e.g., 2020. 
 // d. isAvailable (boolean): Indicates if the car is currently available for rent. 
 // The Car class or function constructor should also have a method called toggleAvailability that changes the isAvailable property to its opposite value (true to false, false to true) 
+class Car {
+    constructor(make, model, year, isAvailable) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.isAvailable = isAvailable;
+    }
+  toggleAvailability() {
+    this.isAvailable = !this.isAvailable;
+  }
+}
+const toyota = new Car("Subaru", "forester", 20006, true);
+console.log(toyota.isAvailable);
+toyota.toggleAvailability();
+console.log(toyota.isAvailable);
 // 2) Create a Rental class or function constructor that has the following properties: ● car (Car object): The car that has been rented. 
 // ● renterName (string): The name of the person who rented the car. ● rentalStartDate (Date object): The start date of the rental period. ● rentalEndDate (Date object): The end date of the rental period. 
 // The Rental class or function constructor should also have a method called calculateRentalDuration that returns the rental duration in days. 
+class Rental {
+    constructor(car, renterName, rentalStartDate, rentalEndDate) {
+        Rental.car = car;
+        Rental.renterName = renterName;
+        Rental.rentalStartDate = rentalStartDate;
+        Rental.rentalEndDate = rentalEndDate;
+    }
+    calculateRentalDuration() {
+      const durationInMs = Rental.rentalEndDate - Rental.rentalStartDate;
+      const durationInDays = Math.ceil(durationInMs / (1000 * 60 * 60 * 24));
+      return durationInDays;
+    }
+  }
+  
 // 3) Create an instance of the Car class or function constructor for a car in the inventory. Then, create an instance of the Rental class or function constructor for a rental involving the car you created. Finally, calculate the rental duration using the calculateRentalDuration method.
+const car = new Car("Subaru", "forester", 2016, true);
+console.log({car});
+
+
+
 
 
 
@@ -75,7 +111,8 @@ class Question {
        Quiz.nextQuestion();
     }
    }
-  
-
+   
+    const myQuiz = new Quiz();
+    
 
 
